d66926e8f92b49eb135d130e186ce28a4d421dc9
BehindLoader <simonsuprun@gmail.com>
2019-01-29 21:19:08 +0300

    getter msg.user

--- examples/simple.js
+++ examples/simple.js
@@ -4,10 +4,13 @@ const bot = new Bot({
   token: process.argv[2]
 })
 
-bot.msg.hear(/hello/i, msg => {
-  msg.send('simple message')
+bot.msg.hear(/Hello/i, async msg => {
+  msg.send('Simple message')
 
-  msg.reply('answer')
+  msg.reply('Answer')
+
+  const user = await msg.user
+  msg.send(`Hello, ${user.first_name} ${user.last_name}!`)
 })
 
 bot.msg.poll()
